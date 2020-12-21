43f59601768e00b737f396727678012d6df3aaad
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2020-12-21 16:41:27 +0300

    Listing All Paired Devices

---
+++ Android/BlueMessenger/app/src/main/java/com/omercankoc/bluemessenger/DeviceListActivity.java
@@ -0,0 +1,55 @@
+package com.omercankoc.bluemessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Set;
+
+public class DeviceListActivity extends AppCompatActivity {
+
+    private ListView listViewPairedDevices;
+    private ListView listViewAvailableDevices;
+
+    private ArrayAdapter<String> adapterPairedDevices;
+    private ArrayAdapter<String> adapterAvailableDevices;
+
+    private Context context;
+
+    private BluetoothAdapter bluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+
+        context = this;
+
+        init();
+    }
+
+    private void init(){
+        listViewPairedDevices = findViewById(R.id.listViewPairedDevices);
+        listViewAvailableDevices = findViewById(R.id.listViewAvailableDevices);
+
+        adapterPairedDevices = new ArrayAdapter<String>(context,R.layout.device_list_item);
+        adapterAvailableDevices = new ArrayAdapter<String>(context,R.layout.device_list_item);
+
+        listViewPairedDevices.setAdapter(adapterPairedDevices);
+        listViewAvailableDevices.setAdapter(adapterAvailableDevices);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if(pairedDevices != null && pairedDevices.size() > 0){
+            for(BluetoothDevice device : pairedDevices){
+                adapterPairedDevices.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    }
+}
--- Android/BlueMessenger/app/src/main/java/com/omercankoc/bluemessenger/MainActivity.java
+++ Android/BlueMessenger/app/src/main/java/com/omercankoc/bluemessenger/MainActivity.java
@@ -1,10 +1,17 @@
 package com.omercankoc.bluemessenger;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Context context;
     private BluetoothAdapter bluetoothAdapter;
+
+    private final int LOCATION_PERMISSION_REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.menu_search_devices:
-                Toast.makeText(context,"Clicked Search Devices",Toast.LENGTH_LONG).show();
+                checkPermission();
                 return true;
             case R.id.menu_bluetooth_on_off:
                 enableBluetooth();
@@ -49,6 +58,45 @@ public class MainActivity extends AppCompatActivity {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter == null){
             Toast.makeText(context,"No Bluetooth Found!",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void checkPermission(){
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_PERMISSION_REQUEST);
+        } else {
+            Intent intent = new Intent(context,DeviceListActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == LOCATION_PERMISSION_REQUEST){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(context,DeviceListActivity.class);
+                startActivity(intent);
+            } else {
+                new AlertDialog.Builder(context)
+                        .setCancelable(false)
+                        .setMessage("Location Permission is Required! \n Please Grant!")
+                        .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                checkPermission();
+                            }
+                        })
+                        .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                MainActivity.this.finish();
+                            }
+                        })
+                        .show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
