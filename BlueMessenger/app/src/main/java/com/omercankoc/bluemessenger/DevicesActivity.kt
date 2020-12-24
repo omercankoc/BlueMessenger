93dc9caba1f3450621b42c0aba4dfba21408f232
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2020-12-24 18:30:06 +0300

    Complated
    - Paired Devices
    - Available Devices

---
+++ BlueMessenger/app/src/main/java/com/omercankoc/bluemessenger/DevicesActivity.kt
@@ -0,0 +1,118 @@
+package com.omercankoc.bluemessenger
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
+class DevicesActivity : AppCompatActivity() {
+
+    private lateinit var textViewPaired : TextView
+    private lateinit var textViewAvailable : TextView
+
+    private lateinit var listViewPaired : ListView
+    private lateinit var listViewAvailable : ListView
+
+    private lateinit var adapterPairedDevices : ArrayAdapter<String>
+    private lateinit var adapterAvailableDevices : ArrayAdapter<String>
+
+    private lateinit var context : Context
+
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_devices)
+
+        context = applicationContext
+
+        initialize()
+    }
+
+    //2
+    private fun initialize(){
+        textViewPaired = findViewById(R.id.textViewPaired)
+        textViewAvailable = findViewById(R.id.textViewAvailable)
+        listViewPaired = findViewById(R.id.listViewPaired)
+        listViewAvailable = findViewById(R.id.listViewAvailable)
+
+        adapterPairedDevices = ArrayAdapter<String>(context,R.layout.devices_item)
+        adapterAvailableDevices = ArrayAdapter<String>(context,R.layout.devices_item)
+
+        listViewPaired.adapter = adapterPairedDevices
+        listViewAvailable.adapter = adapterAvailableDevices
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        var pairedDevices : Set<BluetoothDevice> = bluetoothAdapter.bondedDevices
+
+        if(pairedDevices != null && pairedDevices.isNotEmpty()){
+            for(device in pairedDevices){
+                adapterPairedDevices.add(device.name + "\n" +device.address)
+            }
+        }
+
+        // 3
+        var intentFilterFirst : IntentFilter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(bluetoothDeviceListener,intentFilterFirst)
+
+        var intentFilterSecond : IntentFilter = IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+        registerReceiver(bluetoothDeviceListener,intentFilterSecond)
+    }
+
+    // 3 : TEHLIKELI
+    private val bluetoothDeviceListener : BroadcastReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            var action : String = intent?.action!!
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                var bluetoothDevice : BluetoothDevice? = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                if(bluetoothDevice?.bondState != BluetoothDevice.BOND_BONDED){
+                    adapterAvailableDevices.add(bluetoothDevice?.name + "/n" + bluetoothDevice?.address)
+                }
+            } else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
+                if(adapterAvailableDevices.count == 0){
+                    Toast.makeText(context,"No new device found!",Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(context,"Click on the device to start the chat!",Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    //3
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_devices,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // 3
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.menu_devices -> {
+                scanDevices()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    //3
+    private fun scanDevices(){
+        adapterAvailableDevices.clear()
+        Toast.makeText(context,"Scan Started",Toast.LENGTH_LONG).show()
+        if(bluetoothAdapter.isDiscovering){
+            bluetoothAdapter.cancelDiscovery()
+        }
+        bluetoothAdapter.startDiscovery()
+    }
+}
--- BlueMessenger/app/src/main/java/com/omercankoc/bluemessenger/MessengerActivity.kt
+++ BlueMessenger/app/src/main/java/com/omercankoc/bluemessenger/MessengerActivity.kt
@@ -2,18 +2,27 @@ package com.omercankoc.bluemessenger
 
 import android.bluetooth.BluetoothAdapter
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class MessengerActivity : AppCompatActivity() {
 
-    private var context : Context? = null
+    private lateinit var context : Context
 
     private lateinit var bluetoothAdapter : BluetoothAdapter
+
+    private val LOCATION_PERMISSION_REQUEST : Int = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,17 +33,19 @@ class MessengerActivity : AppCompatActivity() {
         initializeBluetooth()
     }
 
+    // 1.
     // Menuyu arayuze bagla...
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_messenger_activity,menu)
         return super.onCreateOptionsMenu(menu)
     }
 
+    // 1.
     // Menudeki itemlerden birini secince ilgili alana yonlendir...
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.menu_search_devices -> {
-                Toast.makeText(context,"Clicked Search Devices",Toast.LENGTH_LONG).show()
+                checkPermission()
                 return true
             }
             R.id.menu_bluetooth_on_off -> {
@@ -47,6 +58,9 @@ class MessengerActivity : AppCompatActivity() {
         }
     }
 
+    //
+
+    // 1.
     private fun initializeBluetooth(){
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if(bluetoothAdapter == null){
@@ -54,11 +68,50 @@ class MessengerActivity : AppCompatActivity() {
         }
     }
 
-    private fun enableBluetooth(){
-        if(bluetoothAdapter.isEnabled){
-            Toast.makeText(context,"Bluetooth Already Enabled!",Toast.LENGTH_LONG).show()
+    // 2.
+    private fun checkPermission(){
+        if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_PERMISSION_REQUEST)
         } else {
+            val intent : Intent = Intent(context,DevicesActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == LOCATION_PERMISSION_REQUEST){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent : Intent = Intent(context,DevicesActivity::class.java)
+                startActivity(intent)
+            } else {
+                val alertDialog : AlertDialog.Builder = AlertDialog.Builder(this@MessengerActivity)
+                alertDialog.setCancelable(false)
+                alertDialog.setMessage("Location Permission is Required! Please Grant!")
+                alertDialog.setPositiveButton("Grant"){_,_-> checkPermission()}
+                alertDialog.setNegativeButton("Deny"){_,_->this.finish()}
+                val alert : AlertDialog = alertDialog.create()
+                alert.show()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    // 1.
+    private fun enableBluetooth(){
+        if(!bluetoothAdapter.isEnabled){
             bluetoothAdapter.enable()
+        }
+
+        // 3.
+        if(bluetoothAdapter.scanMode != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE){
+            var discoveryIntent : Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+            discoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,300)
+            startActivity(discoveryIntent)
         }
     }
 }
